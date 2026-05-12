9ece374a036deffc3b999317009e2fabe4a02ef1
Joao Victor Alcantara <joaovictoralc28@gmail.com>
2026-05-12 16:56:23 -0300

    configuração inicial de navegação e tema

---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: '#5AC8A7',
+  primaryDark: '#2F9C83',
+
+  secondary: '#F6B26B',
+
+  background: '#F7FBFA',
+  white: '#FFFFFF',
+  black: '#1F2933',
+
+  gray100: '#F1F5F4',
+  gray200: '#DDE7E4',
+  gray500: '#718096',
+  gray700: '#2D3748',
+
+  danger: '#E56B6F',
+};
---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const routes = {
+  splash: '/',
+  onboarding: '/onboarding',
+  chooseProfile: '/choose-profile',
+  tutorHome: '/tutor-home',
+  petForm: '/pet-form',
+  petProfile: '/pet-profile',
+  clinicLogin: '/clinic-login',
+  clinicHome: '/clinic-home',
+};
